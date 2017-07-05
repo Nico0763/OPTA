88c1c033519fadda31dbec84af476cdbd751c2d1
Nicolas Arsène <nicolas.arsene@viacesi.fr>
2017-07-05 13:47:25 +0200

    Add register form

--- register.php
+++ register.php
@@ -3,13 +3,12 @@
         <!-- Page Heading/Breadcrumbs -->
         <div class="row">
             <div class="col-lg-12">
-                <h1 class="page-header">Contact
-                    <small>Subheading</small>
+                <h1 class="page-header">Register
                 </h1>
                 <ol class="breadcrumb">
-                    <li><a href="index.html">Home</a>
+                    <li><a href="index.php">Home</a>
                     </li>
-                    <li class="active">Contact</li>
+                    <li class="active">Register</li>
                 </ol>
             </div>
         </div>
@@ -17,77 +16,67 @@
 
         <!-- Content Row -->
         <div class="row">
-            <!-- Map Column -->
-            <div class="col-md-8">
-                <!-- Embedded Google Map -->
-                <iframe width="100%" height="400px" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com/maps?hl=en&amp;ie=UTF8&amp;ll=37.0625,-95.677068&amp;spn=56.506174,79.013672&amp;t=m&amp;z=4&amp;output=embed"></iframe>
-            </div>
+
             <!-- Contact Details Column -->
-            <div class="col-md-4">
-                <h3>Contact Details</h3>
-                <p>
-                    3481 Melrose Place<br>Beverly Hills, CA 90210<br>
-                </p>
-                <p><i class="fa fa-phone"></i>
-                    <abbr title="Phone">P</abbr>: (123) 456-7890</p>
-                <p><i class="fa fa-envelope-o"></i>
-                    <abbr title="Email">E</abbr>: <a href="mailto:name@example.com">name@example.com</a>
-                </p>
-                <p><i class="fa fa-clock-o"></i>
-                    <abbr title="Hours">H</abbr>: Monday - Friday: 9:00 AM to 5:00 PM</p>
-                <ul class="list-unstyled list-inline list-social-icons">
-                    <li>
-                        <a href="#"><i class="fa fa-facebook-square fa-2x"></i></a>
-                    </li>
-                    <li>
-                        <a href="#"><i class="fa fa-linkedin-square fa-2x"></i></a>
-                    </li>
-                    <li>
-                        <a href="#"><i class="fa fa-twitter-square fa-2x"></i></a>
-                    </li>
-                    <li>
-                        <a href="#"><i class="fa fa-google-plus-square fa-2x"></i></a>
-                    </li>
-                </ul>
+
+              <form>
+  <div class="col-md-6">
+                <div class="form-group">
+                  <label for="name">Gender</label>
+                  <input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked>
+                    Male
+                    <input type="radio" name="optionsRadios" id="optionsRadios1" value="option1">
+                    Female
+                </div>
+                  <div class="form-group">
+                    <label for="name">Name</label>
+                    <input type="text" class="form-control" id="name" placeholder="Your name">
+                  </div>
+                  <div class="form-group">
+                    <label for="surname">Lastname</label>
+                    <input type="text" class="form-control" id="lastname" placeholder="Your lastname">
+                  </div>
+                  <div class="form-group">
+                    <label for="address">Address</label>
+                    <input type="text" class="form-control" id="address" placeholder="Your address">
+                  </div>
+                  <div class="form-group">
+                    <label for="pc">Postal Code</label>
+                    <input type="text" class="form-control" id="pc" placeholder="Your postal code">
+                  </div>
+                  <div class="form-group">
+                    <label for="city">City</label>
+                    <input type="text" class="form-control" id="city" placeholder="Your city">
+                  </div>
+                </div>
+                <div class="col-md-6">
+                  <div class="form-group">
+                    <label for="mail">Mail</label>
+                    <input type="email" class="form-control" id="mail" placeholder="Your mail">
+                  </div>
+                  <div class="form-group">
+                    <label for="mail">Confirmation</label>
+                    <input type="email" class="form-control" id="mail_confirmation" placeholder="Confirm your mail">
+                  </div>
+                  <div class="form-group">
+                    <label for="password">Password</label>
+                    <input type="password" class="form-control" id="password" placeholder="Your password">
+                  </div>
+                  <div class="form-group">
+                    <label for="exampleInputPassword1">Confirmation</label>
+                    <input type="password" class="form-control" id="password_confirmation" placeholder="Confirm your password">
+                  </div>
+                  <div class="checkbox">
+                    <label>
+                      <input type="checkbox"> I'm a driver !
+                    </label>
+                  </div>
+                  <button type="submit" class="btn btn-primary">Send</button>
+                </form>
             </div>
         </div>
         <!-- /.row -->
 
-        <!-- Contact Form -->
-        <!-- In order to set the email address and subject line for the contact form go to the bin/contact_me.php file. -->
-        <div class="row">
-            <div class="col-md-8">
-                <h3>Send us a Message</h3>
-                <form name="sentMessage" id="contactForm" novalidate>
-                    <div class="control-group form-group">
-                        <div class="controls">
-                            <label>Full Name:</label>
-                            <input type="text" class="form-control" id="name" required data-validation-required-message="Please enter your name.">
-                            <p class="help-block"></p>
-                        </div>
-                    </div>
-                    <div class="control-group form-group">
-                        <div class="controls">
-                            <label>Phone Number:</label>
-                            <input type="tel" class="form-control" id="phone" required data-validation-required-message="Please enter your phone number.">
-                        </div>
-                    </div>
-                    <div class="control-group form-group">
-                        <div class="controls">
-                            <label>Email Address:</label>
-                            <input type="email" class="form-control" id="email" required data-validation-required-message="Please enter your email address.">
-                        </div>
-                    </div>
-                    <div class="control-group form-group">
-                        <div class="controls">
-                            <label>Message:</label>
-                            <textarea rows="10" cols="100" class="form-control" id="message" required data-validation-required-message="Please enter your message" maxlength="999" style="resize:none"></textarea>
-                        </div>
-                    </div>
-                    <div id="success"></div>
-                    <!-- For success/fail messages -->
-                    <button type="submit" class="btn btn-primary">Send Message</button>
-                </form>
-            </div>
+
 
         <?php require_once("_inc/footer.php"); ?>
