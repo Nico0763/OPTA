a4106a9a624268733b6c33338d5a7ed8c5c3e7f5
Unknown <noemie.patrikeff@viacesi.fr>
2017-07-06 13:41:48 +0200

    Save ride

--- create_ride.php
+++ create_ride.php
@@ -1,7 +1,8 @@
 <?php
 $active = "createride";
 require_once("_inc/header.php"); ?>
-         <style>
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
+        <style>
            #map {
             height: 400px;
             width: 100%;
@@ -23,24 +24,18 @@ require_once("_inc/header.php"); ?>
         <!-- Content Row -->
         <div class="row">
             <!-- Contact Details Column -->
-            <form>
+           
                 <div class="col-md-6">
-                    <div class="form-group">
-                        <label for="departure">Departure</label>
-                        <select class="form-control">
-                            <option>Rouen</option>
-                            <option>Le Havre</option>
-                            <option>Dieppe</option>
-                        </select>
+                    <div class="input-group form-group">
+                        <span class="input-group-addon" id="basic-addon1">Departure</span>
+                        <!--<input type="text" class="form-control" id="departure" placeholder="Departure">-->
+                        <input id="departure" class="form-control" type="text" placeholder="Enter a location">
                     </div>
-                    <div class="form-group">
-                        <label for="arrival">Arrival</label>
-                        <select class="form-control">
-                            <option>Rouen</option>
-                            <option>Le Havre</option>
-                            <option>Dieppe</option>
-                        </select>
-                    </div>
+                    <div class="input-group form-group">
+                        <span class="input-group-addon" id="basic-addon1">Destination</span>
+                        <!--<input type="text" class="form-control" id="departure" placeholder="Departure">-->
+                        <input id="destination" class="form-control" type="text" placeholder="Enter a location">
+                    </div>                    
                     <div class="input-group form-group">
                         <span class="input-group-addon" id="basic-addon1">Departure time</span>
                         <input type="text" class="form-control" id="departuretime" placeholder="00:00:00">
@@ -51,34 +46,121 @@ require_once("_inc/header.php"); ?>
                     </div>
                     <div class="form-group">
                         <label for="group_students">Select a group of students</label>
-                        <select class="form-control">
+                        <select id="groupstudents" class="form-control">
                             <option>Group 1</option>
                             <option>Group 2</option>
                             <option>Group 3</option>
                         </select>
                     </div>
-                    <button type="submit" class="btn btn-primary">Save</button>
+                    <button  onclick="save_ride();" class="btn btn-primary">Save</button>
                 </div>
                 <div class="col-md-6">
+                <label>Destination</label>
                 <div id="map"></div>
                     <script>
-                      function initMap() {
-                        var paris = {lat: 48.858, lng: 2.357};
-                        var map = new google.maps.Map(document.getElementById('map'), {
-                          zoom: 4,
-                          center: paris
-                        });
-                        var marker = new google.maps.Marker({
-                          position: paris,
-                          map: map
-                        });
-                      }
+                        function save_ride(){
+                            var departure = document.getElementById("departure").value;
+                            var destination = document.getElementById('destination').value;
+                            var departuretime = document.getElementById('departuretime').value;
+                            var timeride = document.getElementById('timeride').value;
+                            var groupstudents = document.getElementById('groupstudents').value;
+
+                            if(departure == ""){
+                                alert('Please select a departure.');
+                            }else if(destination == ""){
+                                alert('Please select a destination');
+                            }else if(departuretime == ""){
+                                alert('Please complete the departure time');
+                            }else if(timeride == ""){
+                                alert('Please complete the time ride');
+                            }else if(groupstudents == ""){
+                                alert('Please select a group of students');
+                            }else{
+                                
+                                var variables = "departure="+departure+"&destination="+destination+"&departuretime="+departuretime+"&timeride="+timeride+"&groupstudents="+groupstudents;
+                                alert(variables);
+                                var xmlhttp = new XMLHttpRequest();
+                                xmlhttp.onreadystatechange = function() {
+                                    if (this.readyState == 4 && this.status == 200) {
+                                        alert(this.responseText);                                     
+                                    }
+                                };
+                                xmlhttp.open("POST", "saveride.php", true);
+                                xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+                                xmlhttp.send(variables);
+                            }
+                            
+                        }
+
+                        function initMap() {
+                            var paris = {lat: 48.858, lng: 2.357};
+                            var map = new google.maps.Map(document.getElementById('map'), {
+                              zoom: 4,
+                              center: paris
+                            });
+                            var marker = new google.maps.Marker({
+                              position: paris,
+                              map: map
+                            });
+
+                            var input = document.getElementById('departure');
+                            var autocomplete = new google.maps.places.Autocomplete(input);
+                            autocomplete.bindTo('bounds', map);
+                            var inputdestination = document.getElementById('destination');
+                            var autocomplete = new google.maps.places.Autocomplete(inputdestination);
+                            autocomplete.bindTo('bounds', map);
+
+                            var infowindow = new google.maps.InfoWindow();
+                            var marker = new google.maps.Marker({
+                                map: map,
+                                anchorPoint: new google.maps.Point(0, -29)
+                            });
+
+                            autocomplete.addListener('place_changed', function() {
+                                infowindow.close();
+                                marker.setVisible(false);
+                                var place = autocomplete.getPlace();
+                                if (!place.geometry) {
+                                    window.alert("Autocomplete's returned place contains no geometry");
+                                    return;
+                                }
+                                
+                                // If the place has a geometry, then present it on a map.
+                                if (place.geometry.viewport) {
+                                    map.fitBounds(place.geometry.viewport);
+                                } else {
+                                    map.setCenter(place.geometry.location);
+                                    map.setZoom(17);
+                                }
+                                marker.setIcon(({
+                                    url: place.icon,
+                                    size: new google.maps.Size(71, 71),
+                                    origin: new google.maps.Point(0, 0),
+                                    anchor: new google.maps.Point(17, 34),
+                                    scaledSize: new google.maps.Size(35, 35)
+                                }));
+                                marker.setPosition(place.geometry.location);
+                                marker.setVisible(true);
+                                
+                                var address = '';
+                                if (place.address_components) {
+                                    address = [
+                                      (place.address_components[0] && place.address_components[0].short_name || ''),
+                                      (place.address_components[1] && place.address_components[1].short_name || ''),
+                                      (place.address_components[2] && place.address_components[2].short_name || '')
+                                    ].join(' ');
+                                }
+                            
+                                infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
+                                infowindow.open(map, marker); 
+                            });
+                         }
                     </script>
                     <script async defer
-                        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBvZFcrGyYZT8nzENpiPKuemDOB3e4Cr60&callback=initMap">
+                        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBvZFcrGyYZT8nzENpiPKuemDOB3e4Cr60&libraries=places&callback=initMap">
                     </script>
                 </div>
-            </form>
+            
         </div>
         <!-- /.row -->
 
---
+++ saveride.php
@@ -0,0 +1,31 @@
+<?php
+$departure =  $_POST['departure'];
+$destination = $_POST['destination'];
+$departuretime = $_POST['departuretime'];
+$timeride = $_POST['timeride'];
+$groupstudents = $_POST['groupstudents'];
+
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+try
+{
+    $conn = new PDO("mysql:host=$servername;dbname=OPTA", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $datetime = strtotime($departuretime);
+	$datetime = date('Y-m-d H:i:s',$datetime);
+    $sql = "INSERT INTO ride (r_departure, r_destination, r_timeride, r_departuretime, r_started, r_driverrefunded, r_amount, id_group, id_company)
+    VALUES ('".$departure."', '".$destination."', '".$timeride."', '".$datetime."', 0, 0, 0, 1, 1)";
+    // use exec() because no results are returned
+    $conn->exec($sql);
+    echo "Ride saved !";
+}
+catch(PDOException $e)
+{
+     echo $sql . "<br>" . $e->getMessage();
+}
+
+?>
