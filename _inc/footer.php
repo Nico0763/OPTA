7e2f97240bcd6178765f68a7e2fcce78c90c0602
Nicolas Arsène <nicolas.arsene@viacesi.fr>
2017-07-07 11:35:21 +0200

    Fix jquery

--- _inc/footer.php
+++ _inc/footer.php
@@ -1,8 +1,7 @@
 </div>
 <!-- /.container -->
 
-<!-- jQuery -->
- <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
+
 
 <!-- Bootstrap Core JavaScript -->
 <script src="js/bootstrap.min.js"></script>
